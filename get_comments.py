533e36e86df7f3f5685011ebe460c9ddc798582f
Aymen <aymenfkir23@gmail.com>
2024-01-10 12:17:10 +0100

    organised the output to csv file

--- get_comments.py
+++ get_comments.py
@@ -2,9 +2,10 @@ import googleapiclient.discovery
 import os
 import dotenv
 import requests
+import pandas as pd
 
 
-def classify_the_comment(payload):
+def classify_comment(payload):
     API_URL = "https://api-inference.huggingface.co/models/martin-ha/toxic-comment-model"
     headers = {"Authorization": os.getenv("hugging_face_key")}
     response = requests.post(API_URL, headers=headers, json=payload)
@@ -25,11 +26,26 @@ def Get_comments():
     response = request.execute()
     return response["items"]
 
+def organise_comments(comments):
+    cmts = []
+    labels = []
+    score = []
+    for comment in comments:
+        cmt = comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+        cmts.append(cmt)
+        class_cmt = classify_comment({"inputs":cmt,})
+        if class_cmt[0][0]["score"] > class_cmt[0][1]["score"]:
+            labels.append(class_cmt[0][0]["label"])
+            score.append(class_cmt[0][0]["score"])
+        else:
+            labels.append(class_cmt[0][1]["label"])
+            score.append(class_cmt[0][1]["score"])
+    df = pd.DataFrame({"comment":cmts,"label":labels,"score":score})
+    return df
+
 
 if __name__ == "__main__":
     comments = Get_comments()
-    for comment in comments:
-        cmt = comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-        class_cmt = classify_the_comment({"inputs":cmt,})
-        print(f"{cmt} : {class_cmt}")
+    df = organise_comments(comments)
+    df.to_csv("comments.csv",index=False)
 # start using openai
