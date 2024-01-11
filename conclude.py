2a5d017d71405dc93838ebca78efa1bd28405c7a
Aymen <aymenfkir23@gmail.com>
2024-01-11 10:01:43 +0100

    using open ai

--- conclude.py
+++ conclude.py
@@ -1,18 +1,18 @@
 from pandasai import SmartDataframe
-import pandasai.llm as llm
 import os
 import dotenv
 import pandas as pd
+from pandasai.llm import OpenAI
 dotenv.load_dotenv()
 
 def get_conclusion(df):
     non_toxic = df[df.label == "non-toxic"]
-    falcon = llm.GooglePalm(api_key=os.getenv("google_api_key"))
-    pandas_ai = SmartDataframe(non_toxic,config={"llm":falcon})
-    conclusion = pandas_ai.chat("summirse this and focus on the most import remarks")
+    llm = OpenAI(api_token=os.getenv("openai_api"))
+    pandas_ai = SmartDataframe(non_toxic,config={"llm":llm})
+    conclusion = pandas_ai.chat("remove the usless comments that doesn't add any value")
     return conclusion
 
 if __name__ == "__main__":
     df = pd.read_csv("comments.csv")
     conclu = get_conclusion(df)
-    print(conclu)
+    conclu.to_csv("conclusion.csv")
