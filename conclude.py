1d0d6502eff63f6b23e7bacb161ab6c850a5b1d3
Aymen <aymenfkir23@gmail.com>
2024-01-13 22:19:59 +0100

    generate a graph for the toxicity analysis

--- conclude.py
+++ conclude.py
@@ -9,10 +9,10 @@ def get_conclusion(df):
     non_toxic = df[df.label == "non-toxic"]
     llm = OpenAI(api_token=os.getenv("openai_api"))
     pandas_ai = SmartDataframe(non_toxic,config={"llm":llm})
-    conclusion = pandas_ai.chat("remove the usless comments that doesn't add any value")
+    #these will generate an image 
+    conclusion = pandas_ai.chat("generate a chart based on this data . the chart must explain the score of toxicity of the comment.")
     return conclusion
 
 if __name__ == "__main__":
     df = pd.read_csv("comments.csv")
-    conclu = get_conclusion(df)
-    conclu.to_csv("conclusion.csv")
+    get_conclusion(df)
